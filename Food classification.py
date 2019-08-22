2e20f878de083fdb3a4ac8c75bdae224689df307
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-08-22 19:30:03 +0530

    Add files via upload
    Download model from http://stratospark.com/demos/food-101/

---
+++ Food classification.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 16:10:41 2019
+
+@author: KS5046082
+"""
+
+from IPython.display import HTML, Image
+
+url = 'http://stratospark.com/demos/food-101/'
+el = '<' + 'iframe src="{}"'.format(url) + ' width="100%" height=600>' # prevent notebook render bug
+HTML(el)
+Image('demo.jpg')
+
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import numpy as np
+from scipy.misc import imresize
+
+import os
+from os import listdir
+from os.path import isfile, join
+import shutil
+import stat
+import collections
+from collections import defaultdict
+
+from ipywidgets import interact, interactive, fixed
+import ipywidgets as widgets
+
+import h5py
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+from keras.applications.inception_v3 import preprocess_input
+from keras.models import load_model
+
+
+
+#"file:///C:/Users/KS5046082/Pictures/food-101.tar/food-101/images"
+
+root_dir = 'C:/Users/KS5046082/Pictures/food-101.tar/food-101/images'
+rows = 17
+cols = 6
+fig, ax = plt.subplots(rows, cols, frameon=False, figsize=(15, 25))
+fig.suptitle('Random Image from Each Food Class', fontsize=20)
+sorted_food_dirs = sorted(os.listdir(root_dir))
+for i in range(rows):
+    for j in range(cols):
+        try:
+            food_dir = sorted_food_dirs[i*cols + j]
+        except:
+            break
+        all_files = os.listdir(os.path.join(root_dir, food_dir))
+        rand_img = np.random.choice(all_files)
+        img = plt.imread(os.path.join(root_dir, food_dir, rand_img))
+        ax[i][j].imshow(img)
+        ec = (0, .6, .1)
+        fc = (0, .7, .2)
+        ax[i][j].text(0, -20, food_dir, size=10, rotation=0,
+                ha="left", va="top", 
+                bbox=dict(boxstyle="round", ec=ec, fc=fc))
+plt.setp(ax, xticks=[], yticks=[])
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+
